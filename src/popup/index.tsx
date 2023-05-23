420229bbe797adc65d127a27c37d014022f0c43b
Danny Duan <321603776@qq.com>
2023-05-23 17:31:45 +0800

    init

---
+++ src/popup/index.tsx
@@ -0,0 +1,11 @@
+function Popup() {
+  return (
+    <div className="App">
+      <div className="title">welcome</div>
+      <input type="text" name="" id="" placeholder="enter your openAI key" />
+      <button>save</button>
+    </div>
+  );
+}
+
+export default Popup;
